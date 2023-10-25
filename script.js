f2b6434bf0f2bb47f3cda20e4f5d00fdbb4203b1
Mariyam_Sardar <87272687+Mariyam97@users.noreply.github.com>
2023-10-26 00:39:11 +0530

    script.js

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 let userInput = document.getElementById("date");
-userInput.max = new Date().toISOString().split("T")[0];
 let result = document.getElementById("result");
 
+// Set the maximum date to today
+userInput.max = new Date().toISOString().split("T")[0];
+
 function calculateAge() {
-  let birthDate = new Date(userInput.value);
+  let selectedDate = new Date(userInput.value);
+  let currentDate = new Date();
 
-  let d1 = birthDate.getDate();
-  let m1 = birthDate.getMonth() + 1;
-  let y1 = birthDate.getFullYear();
-
-  let today = new Date();
-
-  let d2 = today.getDate();
-  let m2 = today.getMonth() + 1;
-  let y2 = today.getFullYear();
-
-  let d3, m3, y3;
-  y3 = y2 - y1;
-
-  if (m2 >= m1) {
-    m3 = m2 - m1;
-  } else {
-    y3--;
-    m3 = 12 + m2 - m1;
+  if (selectedDate > currentDate) {
+    result.innerHTML = "Please select a valid date of birth.";
+    return;
   }
 
-  if (d2 >= d1) {
-    d3 = d2 - d1;
-  } else {
-    m3--;
-    d3 = getDaysInMonth(y1, m1) + d2 - d1;
+  let years = currentDate.getFullYear() - selectedDate.getFullYear();
+  let months = currentDate.getMonth() - selectedDate.getMonth();
+  let days = currentDate.getDate() - selectedDate.getDate();
+
+  if (months < 0 || (months === 0 && days < 0)) {
+    years--;
+    months += 12;
   }
 
-  if (m3 < 0) {
-    m3 = 11;
-    y3--;
+  if (days < 0) {
+    const lastDayOfLastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+    days += lastDayOfLastMonth;
+    months--;
   }
-  result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
 
-  function getDaysInMonth(year, month) {
-    return new Date(year, month, 0).getDate();
-  }
+  result.innerHTML = `You are ${years} years, ${months} months, and ${days} days old`;
 }
